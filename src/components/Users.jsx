39b0d6af2ae19ed6fab422023ca5d84a36aea9f2
akhil-haridas <4khilharidas@gmail.com>
2024-06-28 16:05:35 +0530

    action buttons for delete and update

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const UsersList = ({ users ,onClick}) => {
+const UsersList = ({ users ,onSelect}) => {
   return (
     <div>
       {users.length > 0 ? (
         <ul>
           {users.map((user) => (
-            <li key={user.id} onClick={()=> onClick(user)}>
+            <li key={user.id}>
               {user.name} : {user.age}
+              <span onClick={()=> onSelect(user,'update')}>EDIT USER</span>
+              <span onClick={()=> onSelect(user,'delete')}>Delete USER</span>
             </li>
           ))}
         </ul>
