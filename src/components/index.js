333dd25df0099e4a1bcc0521979e6e8885ae4505
akhil-haridas <4khilharidas@gmail.com>
2024-06-28 15:05:10 +0530

    content for the CRUD components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as UsersList } from './Users';
+export { default as CreateUser } from './Create';
+export { default as UpdateUser } from './Update';
+export { default as DeleteUser } from './Delete';
 
