67296a949a2642cd704df3f54caecdb1149f3d02
akhil-haridas <4khilharidas@gmail.com>
2024-06-28 15:34:48 +0530

    component for the create user

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { supabase } from "../supabase";
 
 const CreateUser = () => {
-  return (
-    <div>Create</div>
-  )
-}
+  const [name, setName] = useState(null);
+  const [age, setAge] = useState(null);
 
-export default CreateUser
+  const handleCreateUser = async (e) => {
+    e.preventDefault();
+    if (!name || !age) {
+      window.alert("Please fill the required fields");
+      return;
+    }
+
+    await supabase.from("users").insert({ name, age });
+    console.log("NAME:", name);
+    console.log("AGE:", age);
+  };
+
+  return (
+    <div>
+      <h1>Create User</h1>
+      <form onSubmit={handleCreateUser}>
+        <label>Name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Age</label>
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateUser;
