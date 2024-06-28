d46cb4037560a05506f954bf21e6abd7c1833ed4
akhil-haridas <4khilharidas@gmail.com>
2024-06-28 18:37:08 +0530

    added edit and delete action

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,35 +1,109 @@
 import React from "react";
-import { alpha } from "@mui/material";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  alpha,
+  styled,
+  tableCellClasses,
+} from "@mui/material";
 import Box from "@mui/material/Box";
-
+import { Delete, Edit } from "@mui/icons-material";
 
 const UsersList = ({ users, onSelect }) => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
 
   return (
     <Box
-    id="image"
-    sx={(theme) => ({
-      mt: { xs: 8, sm: 10 },
-      alignSelf: "center",
-      height: { xs: 200, sm: 700 },
-      width: "100%",
-      backgroundImage:
-        theme.palette.mode === "light"
-          ? 'url("/static/images/templates/templates-images/hero-light.png")'
-          : 'url("/static/images/templates/templates-images/hero-dark.png")',
-      backgroundSize: "cover",
-      borderRadius: "10px",
-      outline: "1px solid",
-      outlineColor:
-        theme.palette.mode === "light"
-          ? alpha("#BFCCD9", 0.5)
-          : alpha("#9CCCFC", 0.1),
-      boxShadow:
-        theme.palette.mode === "light"
-          ? `0 0 12px 8px ${alpha("#9CCCFC", 0.2)}`
-          : `0 0 24px 12px ${alpha("#033363", 0.2)}`,
-    })}
-  />
+      id="image"
+      sx={(theme) => ({
+        mt: { xs: 8, sm: 10 },
+        alignSelf: "center",
+        width: "100%",
+        backgroundImage:
+          theme.palette.mode === "light"
+            ? 'url("/static/images/templates/templates-images/hero-light.png")'
+            : 'url("/static/images/templates/templates-images/hero-dark.png")',
+        backgroundSize: "cover",
+        borderRadius: "10px",
+        outline: "1px solid",
+        outlineColor:
+          theme.palette.mode === "light"
+            ? alpha("#BFCCD9", 0.5)
+            : alpha("#9CCCFC", 0.1),
+        boxShadow:
+          theme.palette.mode === "light"
+            ? `0 0 12px 8px ${alpha("#9CCCFC", 0.2)}`
+            : `0 0 24px 12px ${alpha("#033363", 0.2)}`,
+      })}
+    >
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>No</StyledTableCell>
+              <StyledTableCell>Name</StyledTableCell>
+              <StyledTableCell>Age</StyledTableCell>
+              <StyledTableCell align="center">Edit</StyledTableCell>
+              <StyledTableCell align="center">Delete</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((row, index) => (
+              <StyledTableRow key={index}>
+                <StyledTableCell component="th" scope="row">
+                  {index + 1}
+                </StyledTableCell>
+                <StyledTableCell component="th" scope="row">
+                  {row.name}
+                </StyledTableCell>
+                <StyledTableCell component="th" scope="row">
+                  {row.age}
+                </StyledTableCell>
+                <StyledTableCell
+                  component="th"
+                  scope="row"
+                  align="center"
+                  onClick={() => onSelect(row, "update")}
+                >
+                  <Edit />
+                </StyledTableCell>
+                <StyledTableCell
+                  component="th"
+                  scope="row"
+                  align="center"
+                  onClick={() => onSelect(row, "delete")}
+                >
+                  <Delete />
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
 
