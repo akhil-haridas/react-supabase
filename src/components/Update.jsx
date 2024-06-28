979fd37ad1457f92aa438242c4f9ba8d1ff6663b
akhil-haridas <4khilharidas@gmail.com>
2024-06-28 15:48:15 +0530

    component for updating the user

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { supabase } from "../supabase";
 
-const UpdateUser = () => {
+const UpdateUser = ({ user,onUpdate }) => {
+  const [name, setName] = useState(user.name);
+  const [age, setAge] = useState(user.age);
+
+  const handleUpdateUser = async (e) => {
+    e.preventDefault();
+    if (!name || !age) {
+      window.alert("Please fill the required fields");
+      return;
+    }
+    await supabase.from("users").update({ name, age }).eq("id", user.id);
+    onUpdate(null)
+  };
+
   return (
-    <div>Update</div>
-  )
-}
+    <form onSubmit={handleUpdateUser}>
+      <h1>Update User</h1>
+      <input
+        type="text"
+        placeholder="Please enter the new name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Please enter the new age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <button type="submit">Update</button>
+    </form>
+  );
+};
 
-export default UpdateUser
+export default UpdateUser;
