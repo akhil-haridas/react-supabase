bf14b42b4fedbed07e8498b27cdcb989fe196b65
akhil-haridas <4khilharidas@gmail.com>
2024-06-28 17:03:16 +0530

    using mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "./supabase";
 import { CreateUser, DeleteUser, UpdateUser, UsersList } from "./components";
+import PropTypes from "prop-types";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
+import getLPTheme from "./styles/Theme";
 
 const App = () => {
   const [users, setUsers] = useState([]);
@@ -8,6 +17,14 @@ const App = () => {
   const [action, setAction] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const [mode, setMode] = React.useState("light");
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
 
   const fetchUsers = async () => {
     try {
@@ -34,23 +51,66 @@ const App = () => {
   };
 
   const onSubmit = () => {
-    setUser(null)
-    setAction(null)
-    fetchUsers()
-  }
+    setUser(null);
+    setAction(null);
+    fetchUsers();
+  };
 
   return (
-    <>
+    <ThemeProvider theme={true ? LPtheme : defaultTheme}>
+      {action === "update" && <UpdateUser user={user} onSubmit={onSubmit} />}
+      {action === "delete" && <DeleteUser user={user} onSubmit={onSubmit} />}
+      <CssBaseline />
+      <CreateUser
+        onSubmit={onSubmit}
+        mode={mode}
+        toggleColorMode={toggleColorMode}
+      />
       <UsersList users={users} onSelect={onSelect} />
-      <CreateUser onSubmit={onSubmit}/>
-      {action === "update" && (
-        <UpdateUser user={user} onSubmit={onSubmit} />
-      )}
-      {action === "delete" && (
-        <DeleteUser user={user} onSubmit={onSubmit} />
-      )}
-    </>
+    </ThemeProvider>
   );
 };
 
 export default App;
+
+function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100dvw",
+        position: "fixed",
+        bottom: 24,
+      }}
+    >
+      <ToggleButtonGroup
+        color="primary"
+        exclusive
+        value={showCustomTheme}
+        onChange={toggleCustomTheme}
+        aria-label="Platform"
+        sx={{
+          backgroundColor: "background.default",
+          "& .Mui-selected": {
+            pointerEvents: "none",
+          },
+        }}
+      >
+        <ToggleButton value>
+          <AutoAwesomeRoundedIcon sx={{ fontSize: "20px", mr: 1 }} />
+          Custom theme
+        </ToggleButton>
+        <ToggleButton value={false}>Material Design 2</ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
+  );
+}
+
+ToggleCustomTheme.propTypes = {
+  showCustomTheme: PropTypes.shape({
+    valueOf: PropTypes.func.isRequired,
+  }).isRequired,
+  toggleCustomTheme: PropTypes.func.isRequired,
+};
