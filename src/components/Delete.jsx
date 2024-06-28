3a3efc7fccea326a85d68cb49c86b013a5960239
akhil-haridas <4khilharidas@gmail.com>
2024-06-28 16:21:20 +0530

    delete user

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -4,6 +4,7 @@ import { supabase } from "../supabase";
 const DeleteUser = ({ user, onSubmit }) => {
   const handleDeleteUser = async (e) => {
     e.preventDefault();
+    e.stopPropagation()
     await supabase.from("users").delete().eq("id", user.id);
     onSubmit();
   };
@@ -13,9 +14,9 @@ const DeleteUser = ({ user, onSubmit }) => {
       <h1>Delete User</h1>
       <p>Name : {user.name}</p>
       <p>Age : {user.age}</p>
-      <button type="button" onClick={handleDeleteUser}>
+      <p onClick={handleDeleteUser}>
         Delete
-      </button>
+      </p>
     </div>
   );
 };
