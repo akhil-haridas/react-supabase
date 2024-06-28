5d5a0e72518493c6d4c4f5c46084026ef4376141
akhil-haridas <4khilharidas@gmail.com>
2024-06-28 16:13:55 +0530

    functionality for the delete

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { supabase } from "../supabase";
 
-const DeleteUser = () => {
+const DeleteUser = ({ user, onSubmit }) => {
+  const handleDeleteUser = async (e) => {
+    e.preventDefault();
+    await supabase.from("users").delete().eq("id", user.id);
+    onSubmit(null);
+  };
+
   return (
-    <div>DeleteUser</div>
-  )
-}
+    <div>
+      <h1>Delete User</h1>
+      <p>Name : {user.name}</p>
+      <p>Age : {user.age}</p>
+      <button type="button" onClick={handleDeleteUser}>
+        Delete
+      </button>
+    </div>
+  );
+};
 
-export default DeleteUser
+export default DeleteUser;
