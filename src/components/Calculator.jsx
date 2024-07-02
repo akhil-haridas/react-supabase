c737d5f338fd41c30f19c72f3d3c2e66d08fbb0a
akhil-haridas <4khilharidas@gmail.com>
2024-07-02 17:21:43 +0530

    handle expression click

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -20,6 +20,22 @@ const CalculatorButton = styled(Button)(({ theme }) => ({
 }));
 
 const Calculator = () => {
+
+    const [lhs, setLhs] = useState("");
+    const [expression, setExpression] = useState("lhs");
+    const [opertaor, setOperator] = useState("+");
+    const [rhs, setRhs] = useState("");
+    const [result, setResult] = useState("0");
+
+    const handleExpression = (value) => {
+        if (["+", "-", "/", "*"].includes(value)) {
+            setOperator(value);
+            setExpression("rhs")
+        } else {
+            if (expression === "lhs") setLhs((prev) => prev + value);
+            else if (expression === "rhs") setRhs((prev) => prev + value);
+        }
+    };
     return (
         <Box sx={{ flexGrow: 1, marginTop: "100px", width: "100%" }}>
             <AppBar
@@ -47,25 +63,28 @@ const Calculator = () => {
                         <Grid container>
                             {calculateExpressions.map((exp, index) => (
                                 <Grid item xs={3} key={index}>
-                                    <CalculatorButton variant="contained" color={exp.color}>
+                                    <CalculatorButton variant="contained" color={exp.color} onClick={() => handleExpression(exp.value)}>
                                         {exp.value}
                                     </CalculatorButton>
                                 </Grid>
                             ))}
+                            <CalculatorButton variant="contained" color="error" onClick={() => handleExpression()}>
+                                AC
+                            </CalculatorButton>
                         </Grid>
                     </Grid>
                     <Grid item container xs={6} gap={5}>
                         <Grid container gap={5} justifyContent={"space-around"}>
-                            <Typography variant="h4">11</Typography>
-                            <Typography variant="h4">+</Typography>
-                            <Typography variant="h4">22</Typography>
-                            <Button variant="contained" color="success">
+                            <Typography variant="h4" sx={{ cursor: 'pointer', '&:hover': { transform: 'scale(1.1)', color: 'gray' } }} onClick={() => setExpression("lhs")}>{!lhs ? 0 : lhs}</Typography>
+                            <Typography variant="h4">{opertaor}</Typography>
+                            <Typography variant="h4" sx={{ cursor: 'pointer', '&:hover': { transform: 'scale(1.1)', color: 'gray' } }} onClick={() => setExpression("rhs")}>{!rhs ? 0 : rhs}</Typography>
+                            <Button variant="contained" disabled={!lhs || !rhs} color="success">
                                 ANSWER
                             </Button>
                         </Grid>
                         <Grid container gap={5} justifyContent={"center"}>
                             <Typography variant="h1">=</Typography>
-                            <Typography variant="h1">33</Typography>
+                            <Typography variant="h1">{result}</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
