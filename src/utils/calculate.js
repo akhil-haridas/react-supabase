ed9ca72b4d098c95fdbf0b3afb4012351fd4b172
akhil-haridas <4khilharidas@gmail.com>
2024-07-02 16:39:10 +0530

    util for the calculate exp options

---
+++ src/utils/calculate.js
@@ -0,0 +1,18 @@
+export const calculateExpressions = [
+    { value: "1", color: "info" },
+    { value: "2", color: "info" },
+    { value: "3", color: "info" },
+    { value: "+", color: "warning" },
+    { value: "4", color: "info" },
+    { value: "5", color: "info" },
+    { value: "6", color: "info" },
+    { value: "-", color: "warning" },
+    { value: "7", color: "info" },
+    { value: "8", color: "info" },
+    { value: "9", color: "info" },
+    { value: "*", color: "warning" },
+    { value: "0", color: "info" },
+    { value: ".", color: "warning" },
+    { value: "/", color: "warning" },
+    { value: "AC", color: "error" },
+];
